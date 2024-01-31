c2141ed0b0f94d070f0cc55f5a132cb765cf1b69
ishaj1 <93831881+ishaj1@users.noreply.github.com>
2024-01-31 15:20:42 -0500

    Update database.sql

---
+++ database.sql
@@ -0,0 +1,52 @@
+CREATE TABLE Company(
+    company_username varchar(50) primary key,
+    company_password varchar(50) not null,
+    company_name varchar(50) not null,
+    contact_name varchar(50) not null,
+    contact_detail varchar(50) not null,
+    company_details varchar(500) not null,
+    green_credits float(20,3) not null,
+    funds_required float(20,3)
+);
+
+CREATE TABLE Company_eval(
+    company_username varchar(50),
+    company_size int not null,
+    green_credits float(20,3) not null,
+    -- To be determined later
+    primary key (company_username)
+    foreign key (company_username) references Company(company_username)
+    foreign key (green_credits) references Company(green_credits)
+);
+
+CREATE TABLE Project{
+    project_username varchar(50) primary key,
+    project_password varchar(50) not null,
+    project_name varchar(50) not null,
+    -- Project association refers to what university or lab they work with to prevent unverified projects to register on the platform
+    project_association varchar(50) not null,
+    contact_name varchar(50) not null,
+    contact_detail varchar(50) not null,
+    project_details varchar(500) not null,
+    funds_required float(20,3) not null
+};
+
+CREATE Company_Transaction{
+    transaction_name varchar(50),
+    sender_username varchar(50),
+    receiver_username varchar(50),
+    amount_transferred varchar(50) not null,
+    primary key (transaction_name, sender_username, receiver_username)
+    foreign key (sender_username) references Company(company_username)
+    foreign key (receiver_username) references Company(company_username)
+};
+
+CREATE Project_Transaction{
+    transaction_name varchar(50),
+    sender_username varchar(50),
+    receiver_username varchar(50),
+    amount_transferred varchar(50) not null,
+    primary key (transaction_name, sender_username, receiver_username)
+    foreign key (sender_username) references Company(company_username)
+    foreign key (receiver_username) references Project(project_username)
+};
