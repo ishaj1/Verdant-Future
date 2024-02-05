4ba0da8b48d144727a8ee281041e6bfa876b02f8
Jason <70929180+jasonliny@users.noreply.github.com>
2024-02-05 16:34:34 -0500

    Add test mode publishable key for Stripe

--- frontend/src/pages/Transaction.jsx
+++ frontend/src/pages/Transaction.jsx
@@ -5,7 +5,9 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../components/CheckoutForm";
 
 // stripe publishable API key
-const stripePromise = loadStripe("...");
+const stripePromise = loadStripe(
+  "pk_test_51Oe5AZKlgwtgt0eBV0imAdORghf8yPgLh9waSRIJObsxWKuxjBJmD5BmLMy6FV9pfzclVktrXSF6rwY0dbuDFSVc003mVdnO3P"
+);
 
 export default function Transaction() {
   const [clientSecret, setClientSecret] = useState("");
