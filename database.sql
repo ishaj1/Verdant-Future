28f3272a04d64e0a884a01810d4cb70d9e4130b3
ishaj1 <93831881+ishaj1@users.noreply.github.com>
2024-02-14 13:44:53 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Company(
-    company_username varchar(50) primary key,
+    company_username varchar(50) not null,
     company_password varchar(50) not null,
     company_name varchar(50) not null,
     contact_name varchar(50) not null,
@@ -8,20 +8,21 @@ CREATE TABLE Company(
     green_credits float(20,3) not null,
     funds_required float(20,3),
     funds_received float(20,3),
-    payment_id varchar(200) primary key
+    payment_id varchar(200) not null,
+    primary key(company_username, green_credits, payment_id)
 );
 
 CREATE TABLE Company_eval(
-    company_username varchar(50),
+    company_username varchar(50) not null,
     company_size int not null,
     green_credits float(20,3) not null,
     -- To be determined later
-    foreign key (company_username) references Company(company_username),
-    foreign key (green_credits) references Company(green_credits)
+    primary key (company_username, green_credits),
+    foreign key (company_username, green_credits) references Company(company_username, green_credits)
 );
 
 CREATE TABLE Project(
-    project_username varchar(50) primary key,
+    project_username varchar(50) not null,
     project_password varchar(50) not null,
     project_name varchar(50) not null,
     -- Project association refers to what university or lab they work with to prevent unverified projects to register on the platform
@@ -31,21 +32,20 @@ CREATE TABLE Project(
     project_details varchar(500) not null,
     funds_required float(20,3) not null,
     funds_received float(20,3),
-    payment_id varchar(200) primary key
+    payment_id varchar(200) not null,
+    primary key(project_username, payment_id)
 );
 
 CREATE TABLE Company_Transaction(
-    payer_id varchar(200),
-    payee_id varchar(200),
+    payer_id varchar(200) not null,
+    payee_id varchar(200) not null,
     transaction_name varchar(50) not null,
-    sender_username varchar(50),
-    receiver_username varchar(50),
+    sender_username varchar(50) not null,
+    receiver_username varchar(50) not null,
     amount_transferred varchar(50) not null,
-    primary key (transaction_name),
-    foreign key (sender_username) references Company(company_username),
-    foreign key (receiver_username) references Company(company_username),
-    foreign key (payer_id) references Company(payment_id),
-    foreign key (payee_id) references Company(payment_id)
+    primary key (transaction_name, sender_username, receiver_username),
+    foreign key (sender_username, payer_id) references Company(company_username, payment_id),
+    foreign key (receiver_username, payee_id) references Company(company_username, payment_id)
 );
 
 CREATE TABLE Project_Transaction(
@@ -55,9 +55,7 @@ CREATE TABLE Project_Transaction(
     sender_username varchar(50),
     receiver_username varchar(50),
     amount_transferred varchar(50) not null,
-    primary key (transaction_name),
-    foreign key (sender_username) references Company(company_username),
-    foreign key (receiver_username) references Project(project_username),
-    foreign key (payer_id) references Company(payment_id),
-    foreign key (payee_id) references Company(payment_id)
+    primary key (transaction_name, payer_id, payee_id),
+    foreign key (sender_username, payer_id) references Company(company_username, payment_id),
+    foreign key (receiver_username, payee_id) references Project(company_username, payment_id)
 );
