ff9c13c8c59de424e7517ad17c54c541891d2988
Jason <70929180+jasonliny@users.noreply.github.com>
2024-02-14 22:17:32 -0500

    Update profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router";
 import Header from "../components/Header";
+import { useEffect, useState } from "react";
+import axios from "../api/axios";
 
 const profile = {
   name: "name",
@@ -8,12 +10,45 @@ const profile = {
 
 export default function ProfilePage() {
   const uid = useParams().id;
+  const [profileData, setProfileData] = useState({});
+  const [errors, setErrors] = useState("");
+
+  const queryProfileData = (username) => {
+    axios
+      .post("/display_company_profile", {
+        company_username: username,
+      })
+      .then((response) => {
+        if (response.data.message == "No company found!") {
+          setErrors("User Not Found");
+        } else {
+          return response.data;
+        }
+      })
+      .catch((error) => {
+        setErrors("Error retrieveing profile data. Please try again later.");
+      });
+  };
+
+  useEffect(() => {
+    setProfileData(queryProfileData());
+  });
+
   return (
     <>
       <Header />
-
-      <h1>{profile.name}</h1>
-      <h2>ID: {uid}</h2>
+      {errors}
+      {profileData && (
+        <>
+          <h1>{profileData.company_name}</h1>
+          <p>Green Credits: {profileData.green_credits}</p>
+          <p>Funding Goal: {profileData.funds_required}</p>
+          <p>Username: {profileData.company_username}</p>
+          <p>Contact Name: {profileData.contact_name}</p>
+          <p>Contact Details: {profileData.contact_details}</p>
+          <p>Description: {profileData.company_details}</p>
+        </>
+      )}
     </>
   );
 }
