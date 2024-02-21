7a1b63a9fb39c8f2ec39fdf0778c62cc1aa7dfc3
Shuhua <sl9101@nyu.edu>
2024-02-21 11:09:35 -0500

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,3 @@
-
 import pymysql.cursors
 from flask import (
     Flask,
@@ -9,6 +8,7 @@ from flask import (
     redirect,
     jsonify,
 )
+
 from flask_cors import CORS
 import hashlib
 
@@ -27,10 +27,10 @@ conn = pymysql.connect(
 )
 
 # Constants for referencing
-EMISSION_INTENSITY = 3.671
-ENERGY_INTENSITY = 3.4
-WATER_EFFICIENCY = 50
-WASTE_DIVERSE_RATE = 0.75
+EMISSION_INTENSITY = 3.671 # metric tons per capita
+ENERGY_INTENSITY = 3.4 # megajoules (MJ) per USD
+WATER_EFFICIENCY = 50 # USD / cubic meter
+WASTE_DIVERSE_RATE = 0.75 # percent recycled
 
 # Helper function to calculation evaluation for each of the four categories
 ## if company_input = benchmark, the rating will be 50 (initital rating)
@@ -55,32 +55,41 @@ def get_evaluated():
 
     # Grabs info from the form
     ## General info
-    company_size = request.form['company_size'] # number of employee
-    revenue = request.form['revenue']
+    company_size = int(request.form['company_size']) # number of employee
+    revenue = int(request.form['revenue'])
     ## Greenhouse Gas Emission
-    emission = request.form['emission']
+    emission = int(request.form.get('emission', '0'))
     ## Energy
-    energy = request.form['energy']
+    electricity = int(request.form.get('electricity', '0')) # in kWh
+    e_convert = 3.6 # conversion unit for electricity: 1 kWh = 3.6 MJ
+    natural_gas = int(request.form.get('natural_gas', '0')) # in cf 
+    ng_convert = 1.055 # conversion unit for natural gas: 1 cf = 1.055 MJ
     ## Water
-    water = request.form['water']
+    water = int(request.form.get('water', '0'))
     ## Waste
-    waste = request.form['waste']
-    recycled = request.form['recycled']
+    waste = int(request.form.get('waste', '0'))
+    recycled = int(request.form.get('recycled', '0'))
 
     # Calculate rating for each categories
     ## Greenhouse Gas Emission
     ghg_ratio = round(emission / company_size, 3)
     ghg_rating = calculate_rating(ghg_ratio, EMISSION_INTENSITY, negative=True)
     ## Energy
+    energy = e_convert * electricity + ng_convert * natural_gas
     energy_ratio = round(energy / revenue, 3)
     energy_rating = calculate_rating(energy_ratio, ENERGY_INTENSITY, negative=True)
     ## Water
-    water_ratio = round(revenue / water, 3)
-    water_rating = calculate_rating(water_ratio, WATER_EFFICIENCY, negative=False)
+    water_rating = 0
+    if(water > 0):
+        water_ratio = round(revenue / water, 3)
+        water_rating = calculate_rating(water_ratio, WATER_EFFICIENCY, negative=False)
+        
     ## Waste
-    waste_ratio = round(recycled / waste, 3)
-    waste_rating = calculate_rating(waste_ratio, WASTE_DIVERSE_RATE, negative=False)
-
+    waste_rating = 0
+    if(waste > 0):
+        waste_ratio = round(recycled / waste, 3)
+        waste_rating = calculate_rating(waste_ratio, WASTE_DIVERSE_RATE, negative=False)
+        
     # Final scoring (green credits) = GHG (35%) + Energy (30%) + Water (15%) + Waste (20%)
     # Green Company level: green credits >= 50
     final_rating = 0.35*ghg_rating + 0.3*energy_rating + 0.15*water_rating + 0.2*waste_rating
