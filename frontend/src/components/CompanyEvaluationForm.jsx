ce5628d2bafa260d881c36bbe2238b9b2f2edbde
Jason <70929180+jasonliny@users.noreply.github.com>
2024-03-13 13:42:32 -0400

    Fix import statements

--- frontend/src/components/CompanyEvaluationForm.jsx
+++ frontend/src/components/CompanyEvaluationForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import axios from "./api/axios";
+import axios from "../api/axios";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
+import useAuth from "../hooks/useAuth";
 
 function CompanyEvaluationForm() {
   const currentDate = new Date();
