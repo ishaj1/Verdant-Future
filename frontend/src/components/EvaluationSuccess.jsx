a6a7efba15e00b3a1e9584a0799ff767d8ddcc10
Jason <70929180+jasonliny@users.noreply.github.com>
2024-03-25 12:52:42 -0400

    Add header to top of page

--- frontend/src/components/EvaluationSuccess.jsx
+++ frontend/src/components/EvaluationSuccess.jsx
@@ -30,17 +30,22 @@ function EvaluationSuccess() {
   }, []);
 
   return (
-    <div className="EvaluationSuccess">
-      <h2>Evaluation Successful</h2>
-      {errors && <p>{errors}</p>}
-      {greenCredit && (
-        <div>
-        <p>Your Company's Calculated Green Credit is: {greenCredit.green_credits}</p>
-        {/* Go back to whatever page, subject to change */}
-        <Link to="/">Go to Home Page</Link> 
-        </div> 
-      )}
-    </div>
+    <>
+      <Header />
+      <div className="EvaluationSuccess">
+        <h2>Evaluation Successful</h2>
+        {errors && <p>{errors}</p>}
+        {greenCredit && (
+          <div>
+            <p>
+              Your Company's Calculated Green Credit is: {greenCredit.green_credits}
+            </p>
+            {/* Go back to whatever page, subject to change */}
+            <Link to="/">Go to Home Page</Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
