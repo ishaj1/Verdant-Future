78d88daa36118ed12974611f2657fd1ceb61f391
Jason <70929180+jasonliny@users.noreply.github.com>
2024-04-24 14:40:53 -0400

    Fix name of transfer_status

--- database.sql
+++ database.sql
@@ -52,7 +52,7 @@ CREATE TABLE Company_Transaction(
     receiver_username varchar(50) not null,
     amount_transferred varchar(50) not null,
     credits_transferred varchar(50) not null,
-    transfer_status varchar(10) default 'pending' check (status in ('pending', 'accepted', 'declined', 'canceled')),
+    transfer_status varchar(10) default 'pending' check (transfer_status in ('pending', 'accepted', 'declined', 'canceled')),
     primary key (transaction_name, sender_username, receiver_username),
     foreign key (sender_username, payer_id) references Company(company_username, payment_id),
     foreign key (receiver_username, payee_id) references Company(company_username, payment_id)
