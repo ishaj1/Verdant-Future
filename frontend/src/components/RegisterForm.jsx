bd84b430530bb4c223c6634a02e2fd313710322d
Shuhua <sl9101@nyu.edu>
2024-04-25 00:11:24 -0400

    redirect to profile after login

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -51,7 +51,7 @@ function RegisterForm() {
       .then((response) => {
         if (response.data.register === true) {
           setAuth({ username, isProject });
-          navigate("/organizations");
+          navigate(`/profile/${username}`);
         } else {
           setErrors("Error registering. Check information submitted.");
         }
