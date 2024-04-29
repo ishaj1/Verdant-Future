3514a3b5bd887524cb5f470198b78ba71254288c
Jason <70929180+jasonliny@users.noreply.github.com>
2024-04-29 11:59:37 -0400

    Add committing changes to database and closing cursor when canceling or declining transactions

--- backend/payment.py
+++ backend/payment.py
@@ -635,9 +635,13 @@ def company_transfer_response():
     return {'message': 'Funds transferred successfully', "success": True}
   elif(action == "declined"):
       cursor.execute("DELETE FROM Company_Transaction WHERE transaction_name = %s", transaction_name)
+      conn.commit()
+      cursor.close()
       return {'message': 'Transaction has been declined by the company', "success": True}
   elif(action == "cancelled"):
       cursor.execute("DELETE FROM Company_Transaction WHERE transaction_name = %s", transaction_name)
+      conn.commit()
+      cursor.close()
       return {'message': 'Transaction has been cancelled by the company', "success": True}
   
 @app.route('/get_past_transactions', methods=['GET'])
