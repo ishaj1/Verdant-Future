a91d5d9b55f7be50f96d8319ccc33dcf1a875736
Shuhua <sl9101@nyu.edu>
2024-05-01 20:20:39 -0400

    Update LoginForm.test.js

--- frontend/src/tests/components/LoginForm.test.js
+++ frontend/src/tests/components/LoginForm.test.js
@@ -20,6 +20,7 @@ describe('LoginForm', () => {
 
   it('handles login successfully', async () => {
     axios.post.mockResolvedValueOnce({ data: { user: true } });
+    const header = {"headers": { "Content-Type": "application/x-www-form-urlencoded" }}
     render(<Router><LoginForm /></Router>);
 
     fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'testuser' } });
@@ -28,41 +29,43 @@ describe('LoginForm', () => {
     fireEvent.submit(screen.getByRole('button', { name: 'Login' }));
     expect(axios.post).toHaveBeenCalledTimes(1);
 
-    // await waitFor(() => {
-    //   expect(screen.queryByText('Incorrect login information')).toBeNull();
-    //   expect(screen.queryByText('Error logging in')).toBeNull();
-    //   expect(axios.post).toHaveBeenCalledTimes(1);
-    //   expect(axios.post).toHaveBeenCalledWith("/organizations");
-    // });
+    await waitFor(() => {
+      expect(screen.queryByText('Incorrect login information. Please check your username and password, and try again.')).toBeNull();
+      expect(screen.queryByText('Error logging in. Please try again.')).toBeNull();
+      expect(axios.post).toHaveBeenCalledWith("/login", {"isCompany":false, "username":"testuser", "password":"testpassword"}, header);
+    });
   });
 
-//   it('handles login failure', async () => {
-//     axios.post.mockRejectedValueOnce();
-//     render(<LoginForm />);
+  it('handles login failure', async () => {
+    axios.post.mockResolvedValueOnce({ data: { user: false } });
+    const header = {"headers": { "Content-Type": "application/x-www-form-urlencoded" }}
+    render(<Router><LoginForm /></Router>);
 
-//     fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'testuser' } });
-//     fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'testpassword' } });
+    fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'testuser' } });
+    fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'testpassword' } });
 
-//     fireEvent.submit(screen.getByTestId('login-form'));
+    fireEvent.submit(screen.getByRole('button', { name: 'Login' }));
+    expect(axios.post).toHaveBeenCalledTimes(1);
 
-//     await waitFor(() => {
-//       expect(screen.getByText('Incorrect login information')).toBeInTheDocument();
-//       expect(screen.queryByText('Error logging in')).toBeNull();
-//     });
-//   });
+    await waitFor(() => {
+      expect(screen.getByText('Incorrect login information. Please check your username and password, and try again.')).toBeInTheDocument();
+      expect(screen.queryByText('Error logging in. Please try again.')).toBeNull();
+      expect(axios.post).toHaveBeenCalledWith("/login", {"isCompany":false, "username":"testuser", "password":"testpassword"}, header);
+    });
+  });
 
-//   it('handles network error', async () => {
-//     axios.post.mockRejectedValueOnce(new Error('Network Error'));
-//     render(<LoginForm />);
+  it('handles network error', async () => {
+    axios.post.mockRejectedValueOnce(new Error('Network Error'));
+    render(<Router><LoginForm /></Router>);
 
-//     fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'testuser' } });
-//     fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'testpassword' } });
+    fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'testuser' } });
+    fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'testpassword' } });
 
-//     fireEvent.submit(screen.getByTestId('login-form'));
+    fireEvent.submit(screen.getByRole('button', { name: 'Login' }));
 
-//     await waitFor(() => {
-//       expect(screen.getByText('Error logging in')).toBeInTheDocument();
-//       expect(screen.queryByText('Incorrect login information')).toBeNull();
-//     });
-//   });
+    await waitFor(() => {
+      expect(screen.getByText('Error logging in. Please try again.')).toBeInTheDocument();
+      expect(screen.queryByText('Incorrect login information. Please check your username and password, and try again.')).toBeNull();
+    });
+  });
 });
