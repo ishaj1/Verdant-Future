619ff3fd7e43c2a0c64ed084ba06c2ce9078a327
ShenyiHuang <moucangcang@gmail.com>
2024-05-04 01:46:11 -0400

    eval success

--- frontend/src/pages/TransactionResult.jsx
+++ frontend/src/pages/TransactionResult.jsx
@@ -1,18 +1,35 @@
 import { Link, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import Header from "../components/Header";
+import fav from "../icons/green_credits.png"
 
 export default function TransactionResult() {
   const { auth } = useAuth();
   const { state } = useLocation();
   return (
+    <>
     <div>
-      <h1>Payment Successful</h1>
-      <p>
-        <a href={state?.details?.receipt_url} rel="noopener noreferrer" target="_blank">
-          Receipt
-        </a>
-      </p>
-      <Link to={`/profile/${auth.username}`}>Return to Profile</Link>
+      <Header />
+      <div class="bg-gray-100 h-screen flex items-center">
+          <div class="bg-white p-6 h-4/5 md:mx-auto">
+            <img src={fav} className="w-20 h-20 mx-auto my-6"></img>
+            <div class="text-center">
+              <h3 class="md:text-2xl text-base text-gray-900 font-semibold text-center">Payment Successful!</h3>
+              <p class="text-gray-600 my-2 mt-5">Thank you for supporting a green future for everyone.</p>
+              <p class="text-customGreen-600 my-2 mb-5 hover:text-gray-600">
+                <a href={state?.details?.receipt_url} rel="noopener noreferrer" target="_blank">
+                  View Receipt
+                </a>
+              </p>
+            </div>
+            <div class="py-12 text-center">
+              <Link to={`/profile/${auth?.username}`} className="m-10 rounded-md bg-customGreen-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-customGreen-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Return to Profile
+              </Link>
+            </div>
+          </div>
+      </div>
     </div>
+    </>
   );
 }
