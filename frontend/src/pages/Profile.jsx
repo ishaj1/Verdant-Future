29d73e7478375ba44e555cdcff9ca3a0d749a588
Jason <70929180+jasonliny@users.noreply.github.com>
2024-05-05 23:55:46 -0400

    Add return message

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -23,7 +23,8 @@ export default function ProfilePage() {
   const [errors, setErrors] = useState();
   const [showPasswordForm, setShowPasswordForm] = useState(false);
   const location = useLocation();
-  const [message, setMessage] = useState(location.state && location.state.message);
+  const [returnMessage, setReturnMessage] = useState();
+  const message = location.state && location.state.message;
   const [stripeAccountURL, setStripeAccountURL] = useState("");
 
   const queryProfileData = (username, isProject) => {
@@ -137,6 +138,45 @@ export default function ProfilePage() {
               </strong>
               Please wait for a response from the organization.
             </p>
+          </div>
+          <div className="flex flex-1 justify-end">
+            <button
+              type="button"
+              className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+            >
+              <span className="sr-only">Dismiss</span>
+            </button>
+          </div>
+        </div>
+      )}
+      {returnMessage && (
+        <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+          <div
+            className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+            aria-hidden="true"
+          >
+            <div
+              className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+              style={{
+                clipPath:
+                  "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+              }}
+            />
+          </div>
+          <div
+            className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+            aria-hidden="true"
+          >
+            <div
+              className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+              style={{
+                clipPath:
+                  "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+              }}
+            />
+          </div>
+          <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+            <p className="text-sm leading-6 text-gray-900">{returnMessage}</p>
           </div>
           <div className="flex flex-1 justify-end">
             <button
@@ -376,7 +416,7 @@ export default function ProfilePage() {
                           onClick={() => {
                             setShowPasswordForm(true);
                             setErrors();
-                            setMessage();
+                            setReturnMessage();
                           }}
                         >
                           <img
@@ -392,7 +432,7 @@ export default function ProfilePage() {
                             onClick={() => {
                               setShowPasswordForm(true);
                               setErrors();
-                              setMessage();
+                              setReturnMessage();
                             }}
                           >
                             Update Password
@@ -403,7 +443,7 @@ export default function ProfilePage() {
                       {showPasswordForm && (
                         <>
                           <UpdatePasswordForm
-                            setReturnMessage={setMessage}
+                            setReturnMessage={setReturnMessage}
                             setErrorMessage={setErrors}
                             setShowForm={setShowPasswordForm}
                           />
